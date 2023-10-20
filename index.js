cb7412bcbd1a9c6a26a17cecca0d32c6796d8410
assadujjaman112 <assadujjamanmurad120@gmail.com>
2023-10-21 00:12:02 +0600

    created database and added data

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 
@@ -27,6 +27,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const brandCollection = client.db("brandDB").collection("brand")
+
+    app.post("/products", async(req, res) => {
+        const newProduct = req.body;
+        console.log(newProduct);
+        const result = await brandCollection.insertOne(newProduct);
+        res.send(result);
+
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +49,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
